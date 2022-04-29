1679f876dcbb4aa80a79660fd84b2ffb527e102d
Elizabeth Czarny <liz.cz91@gmail.com>
2022-04-29 14:25:36 +0200

    first commit

--- notes-app.js
+++ notes-app.js
@@ -1,9 +1,4 @@
-const notes = [
-  {
-    title: "Title",
-    body: "Body",
-  },
-];
+const notes = [];
 
 const filters = {
   searchText: "",
@@ -13,38 +8,43 @@ const filters = {
 
 const titleInput = document.querySelector("#input-title");
 const bodyInput = document.querySelector("#input-body");
-
 // render last note of array
 
-const lastNote = function (note) {
+const createNote = function (note) {
   note = notes[notes.length - 1];
   const newNoteDiv = document.createElement("div");
   newNoteDiv.setAttribute("class", "note");
+  const noteTextDiv = document.createElement("div");
+  noteTextDiv.setAttribute("class", "note-text");
   const noteTitleEl = document.createElement("h2");
-  noteTitleEl.textContent = note.title;
   const noteBodyEl = document.createElement("p");
+  noteTitleEl.textContent = note.title;
   noteBodyEl.textContent = note.body;
+  noteTextDiv.append(noteTitleEl, noteBodyEl);
   const deleteIcon = document.createElement("i");
   deleteIcon.setAttribute("class", "fa-solid fa-trash");
   deleteIcon.setAttribute("id", "del-btn");
+  newNoteDiv.append(noteTextDiv, deleteIcon);
   document.querySelector("#notes").appendChild(newNoteDiv);
-  newNoteDiv.appendChild(noteTitleEl).appendChild(noteBodyEl);
-  newNoteDiv.appendChild(deleteIcon);
 };
+
+// const lastNote = function (note) {
+//   note = notes[notes.length - 1];
+//   const newNoteDiv = document.createElement("div");
+//   newNoteDiv.setAttribute("class", "note");
+//   const noteTitleEl = document.createElement("h2");
+//   noteTitleEl.textContent = note.title;
+//   const noteBodyEl = document.createElement("p");
+//   noteBodyEl.textContent = note.body;
+// document.querySelector("#notes").appendChild(newNoteDiv);
+//   newNoteDiv.appendChild(noteTitleEl).appendChild(noteBodyEl);
+// };
+
 // reset function
 const reset = function () {
   titleInput.value = "";
   bodyInput.value = "";
 };
-
-// delete from array function
-//get index
-
-//delete function
-// deleteIcon.addEventListener("click", () => {
-//   const deleteFromArray = function () {};
-//   deleteIcon.parentNode.remove();
-// });
 
 // inner search, re-renderes with every input
 const renderNotes = (notes, filters) => {
@@ -58,15 +58,7 @@ const renderNotes = (notes, filters) => {
 
   filteredNotes.forEach((note) => {
     note = notes[notes.length - 1];
-    lastNote();
-    // const newNoteDiv = document.createElement("div");
-    // newNoteDiv.setAttribute("class", "note");
-    // const noteTitleEl = document.createElement("h2");
-    // noteTitleEl.textContent = note.title;
-    // const noteBodyEl = document.createElement("p");
-    // noteBodyEl.textContent = note.body;
-    // document.querySelector("#notes").appendChild(newNoteDiv);
-    // newNoteDiv.appendChild(noteTitleEl).appendChild(noteBodyEl);
+    createNote();
   });
 };
 
@@ -83,12 +75,21 @@ addBtn.addEventListener("click", function () {
       title: titleInput.value,
       body: bodyInput.value,
     });
-    lastNote();
+    createNote();
     reset();
   }
 });
 
 //delete button
+// const getDelInd = function () {
+//   console.log(notes.indexOf(deleteIcon));
+// };
+const editBtn = document.querySelector(".fa-pencil");
+
+editBtn.addEventListener("click", function (e) {
+  // console.log(e.target);
+  document.querySelector("#del-btn").style.fontSize = "16px";
+});
 
 const DelBtn = document.addEventListener("click", function (e) {
   if (e.target.id === "del-btn") {
@@ -97,7 +98,3 @@ const DelBtn = document.addEventListener("click", function (e) {
     }, 200);
   }
 });
-
-// deleteIcon.addEventListener("click", function () {
-//   console.log(e.target);
-// });
