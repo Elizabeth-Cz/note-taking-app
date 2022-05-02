54b90ab189078eeef90e2a87ed2bccbc6e2a4f25
Elizabeth Czarny <liz.cz91@gmail.com>
2022-05-02 16:05:41 +0200

    swatch change experiment

--- notes-app.js
+++ notes-app.js
@@ -6,8 +6,12 @@ const filters = {
 
 // add note to notes array
 
+const editToggle = document.querySelector(".fa-pencil");
+const swatchContainer = document.querySelector(".container-swatches");
 const titleInput = document.querySelector("#input-title");
 const bodyInput = document.querySelector("#input-body");
+// const searchBtn = document.querySelector(".fa-magnifying-glass");
+
 // render last note of array
 
 const createNote = function (note) {
@@ -25,6 +29,14 @@ const createNote = function (note) {
   deleteIcon.setAttribute("class", "fa-solid fa-trash");
   newNoteDiv.append(noteTextDiv, deleteIcon);
   document.querySelector("#notes").appendChild(newNoteDiv);
+  //reset icon toggle
+  if (titleInput.value || bodyInput.value) {
+    document.querySelectorAll(".fa-toggled").forEach((icon) => {
+      icon.classList.remove("fa-toggled");
+    });
+  }
+  //reset swatches container
+  swatchContainer.classList.remove("container-swatches-on");
 };
 
 // reset function
@@ -67,18 +79,84 @@ addBtn.addEventListener("click", function () {
   }
 });
 
-const editToggle = document.querySelector(".fa-pencil");
-
 editToggle.addEventListener("click", function () {
+  swatchContainer.classList.toggle("container-swatches-on");
   document.querySelectorAll(".fa-trash").forEach((icon) => {
     icon.classList.toggle("fa-toggled");
   });
 });
+
+//only removes index 0 of array
 
 const delBtn = document.addEventListener("click", function (e) {
   if (e.target.classList.contains("fa-trash")) {
     setTimeout(function () {
       e.target.parentElement.remove();
     }, 200);
+    console.log(notes);
   }
 });
+
+// function searchVisible() {
+//   if (searchInput.style.display === "none") {
+//     searchInput.style.display = "block";
+//   }
+//    else if (searchInput.style.display === "block") {
+//     searchInput.style.display = "none";
+//   }
+// }
+window.onload = searchVisible;
+document.getElementById("search-icon").addEventListener("click", searchVisible);
+function searchVisible() {
+  searchInput = document.querySelector("#search-text");
+  if (searchInput.style.display == "none") {
+    searchInput.style.display = "inline";
+  } else if ((searchInput.style.display = "inline")) {
+    searchInput.style.display = "none";
+  }
+}
+
+// document.getElementById("search-icon").addEventListener("click", function (e) {
+//   const searchInput = document.querySelector("#search-text");
+
+// if (searchInput.style.display == "none") {
+//   searchInput.style.display = "block";
+// } else if ((searchInput.style.display = "block")) {
+//   searchInput.style.display = "none";
+// }
+//   // console.log(searchInput);
+//   console.log(e);
+// });
+// HAVING FUN
+// const redToggle = document.getElementsByClassName("red");
+// const tealToggle = document.getElementsByClassName("teal");
+// const purpleToggle = document.getElementsByClassName("purple");
+// const yellowToggle = document.getElementsByClassName("yellow");
+// const greenToggle = document.getElementsByClassName("green");
+
+// const swatches = document.querySelectorAll(".swatch");
+// swatches.forEach((swatch) => {
+//   console.log(swatch);
+// });
+
+const container = document.querySelector(".container");
+const changeColor = document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("swatch")) {
+    container.classList.toggle(e.target.classList[1]);
+    console.log(e);
+  }
+  console.log(container.classList);
+});
+// console.log(e.target.classList[1]);
+// console.log(container);
+// const swatchColor = e.target.classList[1];
+// container.setAttribute("class", swatchColor);
+// const getNotes = document.querySelectorAll(".note");
+// getNotes.forEach((note) => {
+//   reset classes to .note
+//   add new note in theme color
+//   note.setAttribute("class", "note");
+//   note.classList.toggle(swatchColor);
+//   console.log(note.classList);
+// });
+// console.log();
